530d17fab06db3d8f7e023dfa3cee0082ebffb4a
Andrea Martins <andmart2@car6s5.42barcelona.com>
2023-05-19 21:36:18 +0200

    190523-1

--- ft_split.c
+++ ft_split.c
@@ -6,48 +6,45 @@
 /*   By: andmart2 <andmart2@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 18:27:15 by andmart2          #+#    #+#             */
-/*   Updated: 2023/05/19 21:11:01 by andmart2         ###   ########.fr       */
+/*   Updated: 2023/05/19 21:34:24 by andmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include "libft.h"
 
-static unsigned int    count_substr(const char *str, char c)
+static unsigned int	count_substr(const char *str, char c)
 {
-    unsigned int     num;
+	unsigned int	num;
 
-    num = 0;
-    while (*str)
-    {
-        while (*str == c)
-            str++;
+	num = 0;
+	while (*str)
+	{
+		while (*str == c)
+			str++;
 		if (*str)
 			num++;
-        while (*str && *str != c)
-            str++;
-    }
-    return (num);
+		while (*str && *str != c)
+			str++;
+	}
+	return (num);
 }
 
 static char	**ft_freematrix(char **matrix, unsigned int i)
 {
-	while(0 < i--)
+	while (0 < i--)
 		free(matrix[i]);
 	free(matrix);
-	return(NULL);
+	return (NULL);
 }
-
 
 char	**ft_split(const char *s, char c)
 {
-	unsigned int	num_substr;
 	unsigned int	i;
 	char			**to_allocate;
 	const char		*start_substr;
 
 	i = 0;
-	num_substr = count_substr(s, c);
-	to_allocate = ft_calloc((num_substr + 1), sizeof(char*));
+	to_allocate = ft_calloc((count_substr(s, c) + 1), sizeof(char *));
 	if (to_allocate == NULL)
 		return (NULL);
 	while (*s)
@@ -61,14 +58,13 @@ char	**ft_split(const char *s, char c)
 				s++;
 			to_allocate[i] = ft_substr(start_substr, 0, s - start_substr);
 			if (!to_allocate[i])
-				return(ft_freematrix(to_allocate,i));
+				return (ft_freematrix(to_allocate, i));
 			i++;
 		}
 	}
 	to_allocate[i] = NULL;
-	return(to_allocate);
+	return (to_allocate);
 }
-
 /*int	main(void)
 {
 
@@ -82,49 +78,13 @@ char	**ft_split(const char *s, char c)
 		for (int j = 0; lol[j]; j++)
 			printf("'%s'\n", lol[j]);
 		printf("\n");
-		lol = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i');
+		lol = ft_split("lorem ipsum dolor sit amet, 
+		consectetur adipiscing elit. Sed non risus.
+	   	Suspendisse lectus tortor,
+	   	dignissim sit amet, adipiscing nec, 
+		ultricies sed, dolor. 
+		Cras elementum ultricies diam. 
+		Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i');
 		for (int k = 0; lol[k]; k++)
 			printf("'%s'\n", lol[k]);
-}*/
-
-
-/*
-//int	main (int argc, char **argv)
-int main(void)
-{
-	const char *s = "holaoondkdnnfjdnvo jndfjofo";
-
-	char c = 'o';
-	//char **result;
-	int i;
-
-	i = 0;
-//	if (argc != 3)
-//	{
-//		printf("error");
-//		return(1);
-//	}
-	
-//	s = argv[1];
-//	c = argv[2][0];
-	
-	ft_split(s,c);
-	printf("abcn");
-//	if (result != NULL)
-//	{
-//		while (result[i] != 0 && i < 5)
-//		{
-//			printf("errorresult%s\n", result[i]);
-//			i++;
-//		}
-//	}	
-	//if (result == NULL)
-	//{
-	//	while (result[i] != 0)
-	//	{
-	//		free(result);
-	//	}
-	//	free(result);
-//	}
-	//return (result);
 }*/
